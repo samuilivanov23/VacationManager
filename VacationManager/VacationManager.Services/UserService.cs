3c421dfdc85899e12acff6fe1ed501ec94f18f87
Samuil Ivanov <samuil2001ivanov@gmail.com>
2020-03-17 00:26:38 +0200

    Documenting the UserService service

--- VacationManager/VacationManager.Services/UserService.cs
+++ VacationManager/VacationManager.Services/UserService.cs
@@ -18,6 +18,18 @@ namespace VacationManager.Services
             this.context = context;
         }
 
+        /// <summary>
+        /// This method is used for the register functionality
+        /// which creates users in the database.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="confirmPassword"></param>
+        /// <param name="email"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
         public int Register(string firstName,
                              string lastName,
                              string username,
@@ -67,14 +79,21 @@ namespace VacationManager.Services
 
             user.Team = null;
 
+            //Saving the changes in the databse.
             context.Users.Add(user);
             context.SaveChanges();
 
             return user.Id;
         }
 
+        /// <summary>
+        /// This method is used to edit a user's information
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public int EditUser(EditUserViewModel model)
         {
+            //Taking the user which is going to be edited
             User takenUser = context.Users.FirstOrDefault(u => u.Id == model.Id);
 
             //validating if the username or email have already been taken
@@ -93,6 +112,7 @@ namespace VacationManager.Services
                 }
             }
 
+            //editing the user's information
             takenUser.FirstName = model.FirstName;
             takenUser.LastName = model.LastName;
             takenUser.Email = model.Email;
@@ -115,15 +135,23 @@ namespace VacationManager.Services
                 takenUser.PermissionLevel = 2;
             }
 
+            //Saving the changes in the database.
             context.Users.Update(takenUser);
             context.SaveChanges();
 
             return takenUser.Id;
         }
 
+        /// <summary>
+        /// This method is used to delete a user from the database.
+        /// </summary>
+        /// <param name="id"></param>
         public void DeleteUser(int id)
         {
+            //Taking the user based on the given id
             User user = context.Users.FirstOrDefault(u => u.Id == id);
+
+            //if the user has a team, he is removed from the team.
             if(user.TeamId != null)
             {
                 Team team = context.Teams.FirstOrDefault(t => t.Id == user.TeamId);
@@ -131,14 +159,25 @@ namespace VacationManager.Services
                 context.Teams.Update(team);
             }
             
+            //Saving the changes from the database.
             context.Users.Remove(user);
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// This method is used to create a login functionality
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public int Login(string username, string password)
         {
+            //Taking the correct user based on the given username and password
             var user = context.Users.FirstOrDefault(x => x.Username == username && x.Password == password);
 
+            //If there is such a user in the database
+            //The logged user id is set to this user's id
+            //Otherwise, the user is asked to type a valid login data.
             if (user != null)
             {
                 LoggedUserInfo.LoggedUserId = user.Id;
@@ -154,6 +193,11 @@ namespace VacationManager.Services
             return loggedUser;
         }
 
+        /// <summary>
+        /// This methot is used to return a single User which is shown in the ViewUser view
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public User ViewUser(int id)
         {
             var users = context.Users.Select(u => new User()
@@ -171,18 +215,34 @@ namespace VacationManager.Services
             return users.FirstOrDefault(u => u.Id == id);
         }
 
+        /// <summary>
+        /// This method is used when a team lead employee wants to add
+        /// a developer without a team to his team.
+        /// </summary>
+        /// <param name="loggedUserId"></param>
+        /// <param name="userToBeAddedId"></param>
         public void AddUserToTeam(int loggedUserId, int userToBeAddedId)
         {
+            //Taking the logged user based on the loggeduserId
             User loggedUser = context.Users.FirstOrDefault(u => u.Id == loggedUserId);
+
+            //Taking the team of the logged user
             Team team = context.Teams.FirstOrDefault(t => t.Id == loggedUser.TeamId);
+
+            //Taking the user which is going to be added to the team based on the userToBeAddedId
             User userToBeAdded = context.Users.FirstOrDefault(u => u.Id == userToBeAddedId);
 
+            //Adding the user to the team and saving the changes to the database
             team.Users.Add(userToBeAdded);
             context.Users.Update(userToBeAdded);
             context.Teams.Update(team);
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// This method returns a model that contains all currently stored users in the database.
+        /// </summary>
+        /// <returns></returns>
         public AllUsersViewModel GetAllUsers()
         {
 
